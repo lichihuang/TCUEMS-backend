3b03848902336bbcf7dced0ac0b4ac6f46330038
Li Chi Huang <71595575+lichihuang@users.noreply.github.com>
2024-03-07 10:14:19 +0800

    Add files via upload

---
+++ TCUEMS_BackendNew/Models/DB_SemesterWarning.cs
@@ -0,0 +1,14 @@
+﻿namespace TCUEMS_BackendNew.Models
+{
+    public class SemesterWarning
+    {
+        public string W_Smtr { get;}
+        public string W_Std_No { get; }
+        public string Chi_Name { get; }
+        public string St_State { get;}
+        public string Dept_Name_S { get; }
+        public string Degree { get; }
+        public string Sw_Class { get; }
+    }
+}
+
