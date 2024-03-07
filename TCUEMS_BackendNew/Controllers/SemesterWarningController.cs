0329ff81220a937b04e52b06685fa87bff07f6a8
Li Chi Huang <71595575+lichihuang@users.noreply.github.com>
2024-03-07 10:11:40 +0800

    Add files via upload

---
+++ TCUEMS_BackendNew/Controllers/SemesterWarningController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TCUEMS_BackendNew.Data;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SemesterWarningController : ControllerBase
+{
+    private readonly ISemesterWarningRepository _semesterWarningRepository;
+
+    public SemesterWarningController(ISemesterWarningRepository semesterWarningRepository)
+    {
+        _semesterWarningRepository = semesterWarningRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetSemesterWarnings()
+    {
+        try
+        {
+            var semesterWarnings = await _semesterWarningRepository.GetAllSemesterWarnings();
+            return Ok(semesterWarnings);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal server error: {ex.Message}");
+        }
+    }
+}
